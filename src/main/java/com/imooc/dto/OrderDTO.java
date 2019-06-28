422607b4322eb596b17f148a1ae3ae34a175c316
bravery <2472937751@qq.com>
2019-06-28 14:12:40 +0800

    6完成

--- src/main/java/com/imooc/dto/OrderDTO.java
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -1,15 +1,20 @@
 package com.imooc.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.enums.OrderStatusEnum;
 import com.imooc.enums.PayStatusEnum;
+import com.imooc.utils.serializer.Date2LongSerialzer;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
     private String orderId;
 
@@ -29,8 +34,10 @@ public class OrderDTO {
     // 支付状态默认为0等待支付
     private Integer payStatus;
 
+    @JsonSerialize(using = Date2LongSerialzer.class)
     private Date createTime;
 
+    @JsonSerialize(using = Date2LongSerialzer.class)
     private Date updateTime;
 
     private List<OrderDetail> orderDetailList;
